8456a6d53cacc1f9ff94a52e23c2c5c4473c6768
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2023-02-18 23:42:03 +0530

    first commit

---
+++ population.py
@@ -0,0 +1,80 @@
+import random
+populations=[]
+for x in range(4):
+    s=[]
+    for y in range(6):
+        s.append(random.randint(0,1))
+    populations.append(s)
+print(populations)
+# Converting binary to decimal
+def bits_to_int(bits):
+    value=0
+    for bit in bits:
+        value=(value<<1)|bit
+    return value
+numeric=[]
+#storing the decimal values in a list
+for x in populations:
+    numeric.append(bits_to_int(x))
+#sorting the list for parent selection
+def selection():
+ for x in range(0,len(populations)):
+    for y in range(0,len(populations)):
+        if bits_to_int(populations[x])>bits_to_int(populations[y]):
+            temp=populations[x]
+            populations[x]=populations[y]
+            populations[y]=temp
+            numeric[x]=bits_to_int(populations[x])
+            numeric[y]=bits_to_int(populations[y])
+selection()
+print(numeric)
+populations2=[]
+# one to one crossover
+def crossover():
+    global populations2
+    r=random.randint(0,len(populations[0])-1)
+    for i in range(0,len(populations),2):
+     populations2.append(populations[i][0:r]+populations[i+1][r:6])
+crossover()
+# crossovered population
+print(populations2)
+bit_flip_mutation=[]
+#bit flip mutation
+def bit_flip():
+    global bit_flip_mutation
+    for i in range(0,len(populations2)):
+        r=random.randint(0,len(populations2[i])-1)
+        bit_flip_mutation.append(populations2[i][0:r]+[1-populations2[i][r]]+populations2[i][r+1:6])
+bit_flip()
+print(bit_flip_mutation)
+swap_mutation=[]
+#swap mutation
+def swap():
+    global swap_mutation
+    for i in range(0,len(populations2)):
+        swap_handler=[]
+        # to prevent extra swaps
+        while(len(swap_handler)!=6):
+         r=random.randint(0,len(populations2[i])-1)
+         r1=random.randint(0,len(populations2[i])-1)
+         swap_handler=populations2[i][0:r]+[populations2[i][r1]]+populations2[i][r+1:r1]+[populations2[i][r]]+populations2[i][r1+1:6]
+        swap_mutation.append(swap_handler)
+swap()
+print(swap_mutation)
+print('Comparison')
+print('Population 1')
+print(populations)
+for x in populations:
+    print(bits_to_int(x))
+print('After crossover')
+print(populations2)
+for x in populations2:
+    print(bits_to_int(x))
+print('After bit flip mutation')
+print(bit_flip_mutation)
+for x in bit_flip_mutation:
+    print(bits_to_int(x))
+print('After swap mutation')
+print(swap_mutation)
+for x in swap_mutation:
+    print(bits_to_int(x))
